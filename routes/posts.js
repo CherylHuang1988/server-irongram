6ed9e4f57e6b99de80f46b59bc8b6624b20ddd9d
André de Albuquerque <itstheandre@gmail.com>
2021-11-06 11:41:34 +0100

    First error handling

--- routes/posts.js
+++ routes/posts.js
@@ -19,4 +19,18 @@ router.get("/", (req, res) => {
   res.json({ posts });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const singlePost = posts.find((element) => element.id === id); // Post.findById(id).then(singlePost => {if (!singlePost) {}})
+  console.log("singlePost:", singlePost);
+
+  if (!singlePost) {
+    return res
+      .status(404)
+      .json({ errorMessage: `Post with the id ${id} does not exist` });
+  }
+
+  res.json({ post: singlePost });
+});
+
 module.exports = router;
