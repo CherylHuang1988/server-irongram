6138fe48854fff3e294f6b3d3f5d007399e500cc
André de Albuquerque <itstheandre@gmail.com>
2021-11-06 10:46:46 +0100

    Initial Flow

---
+++ routes/posts.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+const posts = [
+  {
+    id: "fancy",
+    content: "Im so fancy, you already know. #f4f #friendslearnwithme",
+    image: "https://i.ytimg.com/vi/WCcpSyVWMwU/maxresdefault.jpg",
+  },
+  {
+    id: "absdkasdfgkjhsdf",
+    content: "This is not an image",
+    image:
+      "https://static.wikia.nocookie.net/mrbean/images/4/4b/Mr_beans_holiday_ver2.jpg/revision/latest?cb=20181130033425",
+  },
+];
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.json({ posts });
+});
+
+module.exports = router;
